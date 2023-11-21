aeee18fa93ead47e90453697c5d5817ab0d27e67
2329Lamas-Erick <lamaserick28@gmail.com>
2023-11-21 00:20:20 -0300

    Mas ajustes

--- Final-Movil/src/app/pages/tabs/inicio/inicio.page.ts
+++ Final-Movil/src/app/pages/tabs/inicio/inicio.page.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
-  saldo = 0;
+  saldo: number = 10000;
   constructor() { }
 
   ngOnInit() {
