1df420b5f094073336b86c7eafd8735dd3fdac17
2329Lamas-Erick <lamaserick28@gmail.com>
2023-11-21 01:59:09 -0300

    Más Cambios

--- Final-Movil/src/app/pages/tabs/inicio/inicio.page.ts
+++ Final-Movil/src/app/pages/tabs/inicio/inicio.page.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioPage implements OnInit {
   saldo: number = 10000;
+  options: any[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.options = [
+      {id:1, color:'tertiary', icon: 'paper-plane', name:'Enviar'},
+      {id:2, color:'white', icon: 'send',name: 'Ingresar' },
+      {id:3, color:'success', icon: 'add-circle', name:'Sacar'},
+      {id:4, color:'light', icon:'person', name: 'Info'},
+    ];
   }
 
 }
